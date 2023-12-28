8a29c997bf5423338196fd9e9425d1a3b5eb991f
larry <changyu.liu@alphabetter.com.cn>
2023-12-28 17:23:04 +0800

    upgrade

--- yolov8_pytorch/utils/callbacks/neptune.py
+++ yolov8_pytorch/utils/callbacks/neptune.py
@@ -2,10 +2,18 @@
 
 from yolov8_pytorch.utils import LOGGER, SETTINGS, TESTS_RUNNING
 
-assert not TESTS_RUNNING  # do not log pytest
-assert SETTINGS['neptune'] is True  # verify integration is enabled
-import neptune
-from neptune.types import File
+try:
+    assert not TESTS_RUNNING  # do not log pytest
+    assert SETTINGS['neptune'] is True  # verify integration is enabled
+    import neptune
+    from neptune.types import File
+
+    assert hasattr(neptune, '__version__')
+
+    run = None  # NeptuneAI experiment logger instance
+
+except (ImportError, AssertionError):
+    neptune = None
 
 
 def _log_scalars(scalars, step=0):
